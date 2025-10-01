a2ba8576da2177b4a136a85635bf8ea53190666f
Ghulam Mustafa <157616503+G-Mustafa1@users.noreply.github.com>
2025-10-01 17:27:10 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
  // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
- import { getAuth , RecaptchaVerifier , signInWithPhoneNumber,signOut , onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+ import { getAuth , RecaptchaVerifier , signInWithPhoneNumber,signOut , onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
  // TODO: Add SDKs for Firebase products that you want to use
  // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -27,4 +27,5 @@
     signInWithPhoneNumber ,
     signOut,
     onAuthStateChanged
+
  }
